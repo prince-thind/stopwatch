802aaad90a3b3067a7f695fa6bb70380db2f726a
Prince Thind <Princethind799@gmail.com>
2022-01-29 20:35:41 +0530

    add basic increment

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Header from "./components/Header";
 import formatTime from "./lib/formatTime";
 
 function App() {
-  const [time, setTime] = useState(154525); //time in ms
+  const [time, setTime] = useState(0); //time in ms
+
+  useEffect(() => {
+    const timerID = setInterval(() => {
+      setTime((time) => time + 45);
+    }, 45);
+    return () => {
+      clearInterval(timerID);
+    };
+  }, []);
+
   return (
     <div className="App">
       <Header />
-      <div className="time">
-        {formatTime(time)}
+      <div className="time">{formatTime(time)}</div>
+      <div className="buttons">
+        <button className="button">Start</button>
+        <button className="button">Stop</button>
+        <button className="button">Reset</button>
       </div>
     </div>
   );
