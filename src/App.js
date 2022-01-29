e3e47eab6052fd711cccc59ff52fae8657113f0a
Prince Thind <Princethind799@gmail.com>
2022-01-29 13:24:52 +0530

    scratch commit

--- src/App.js
+++ src/App.js
@@ -1,33 +1,5 @@
-import { useEffect, useState } from 'react';
-import Header from './components/header';
-import Reset from './components/reset';
-import TimeForm from './components/time-form';
-import Timer from './components/timer';
-
 function App() {
-  const [time, setTime] = useState(0);
-
-  useEffect(() => {
-    const timerID = setTimeout(() => {
-      if (time > 0) {
-        setTime((time) => time - 1);
-      }
-    }, 1000);
-
-    return () => {
-      clearTimeout(timerID);
-    };
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [time]);
-
-  return (
-    <div className="App">
-      <Header />
-      <TimeForm setTime={setTime} />
-      <Timer time={time} />
-      <Reset setTime={setTime} />
-    </div>
-  );
+  return <div>00:00</div>;
 }
 
 export default App;
